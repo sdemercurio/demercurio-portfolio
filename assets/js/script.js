e94df48361c5a75bb202454c2c307ca41075cb68
Sarah DeMercurio <sdemercuriomi@gmail.com>
2021-02-01 10:23:38 -0500

    adding 'Thank you' message after submission

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+$("#submit-button").on("click", function () {
+
+    $("#thank-you").append("<div class='submit-message'><h3>Thanks! I'll be in touch</h3></div>");
+
+
+})
+
+})
